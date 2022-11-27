cdd9ff472a62780af2dba366f1ec7990b2f86ed7
jonathanavc <jonvenega@gmail.com>
2022-11-27 16:11:18 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,14 +7,15 @@ using namespace std;
 
 bool _cout = 1;
 
-void _temp_print(string _s){
+void _temp_print(string _s, int _i1 = -1, int _i2 = -1){
+    if( _i1!= -1) cout <<"["<< _i1 <<"/"<<_i2<<"]";
     cout << _s;
     cout << "\e[A";
     cout << "\33[2K\r";
 }
 
 void read_dataset(){
-    for (size_t i = 0; i < 1<<18; i++);
+    for (size_t i = 0; i < 1<<20; i++);
 }
 
 int main(int argc, char const *argv[]){
@@ -22,14 +23,21 @@ int main(int argc, char const *argv[]){
         cout << "Modo de uso: "<< argv[0]<<" \"Nombre directorio\"" <<endl;
         return 1;
     }
-    int num_files = 0;
     string path = argv[1];
-    
+    int num_files = 0;
     if (auto dir = opendir(path.c_str())) {
         while (auto f = readdir(dir)){
             if (!f->d_name || f->d_name[0] == '.') continue;
-            if(_cout)_temp_print((string)f->d_name + "\n");
             num_files++;
+        }
+        closedir(dir);
+    }
+    if (auto dir = opendir(path.c_str())) {
+        int _cont = 0;
+        while (auto f = readdir(dir)){
+            if (!f->d_name || f->d_name[0] == '.') continue;
+            if(_cout)_temp_print("\n", _cont, num_files);
+            _cont++;
             read_dataset();
         }
         closedir(dir);
