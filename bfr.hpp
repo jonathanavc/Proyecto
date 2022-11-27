d23938bc734f908a12afb1dd24ef8b9190f4ec1e
jonathanavc <jonvenega@gmail.com>
2022-11-27 18:34:57 -0300

    (❁´◡`❁)

---
+++ bfr.hpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+
+class brf
+{
+private:
+    /* data */
+public:
+    brf(/* args */);
+    ~brf();
+};
+
+brf::brf(/* args */)
+{
+}
+
+brf::~brf()
+{
+}
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <sstream>
 #include "json.hpp"
+#include "bfr.hpp"
 //#include "word2vec.c"
 
 using namespace std;
@@ -11,29 +12,36 @@ using namespace std;
 bool _cout = 1;
 
 void _temp_print(string _s, int _i1 = -1, int _i2 = -1){
+    cout << "\33[2K\r";
     if( _i1!= -1) cout <<"["<< _i1 <<"/"<<_i2<<"]";
     cout << _s;
     cout << "\e[A";
-    cout << "\33[2K\r";
+    
+    
 }
 
 void read_dataset(string dir){
-    cout << dir << endl;
+    //cout << dir << endl;
     ifstream dataset_file(dir);
     stringstream buffer;
     buffer << dataset_file.rdbuf();
     auto json = nlohmann::json::parse(buffer);
     for (auto text : json){
-        cout << text["title"] << endl;
+        cout << text["text"] << endl;
     }
 }
 
 int main(int argc, char const *argv[]){
-    if(argc != 2){
-        cout << "Modo de uso: "<< argv[0]<<" \"Nombre directorio\"" <<endl;
+    if(argc != 3){
+        cout << "Modo de uso: "<< argv[0]<<" \"Nombre directorio\" \"N°threads\"" <<endl;
         return 1;
     }
     string path = argv[1];
+    int n_threads = atoi(argv[2]);
+    if(n_threads < 0) n_threads = 1;
+    thread threads[n_threads];
+
+
     int num_files = 0;
     if (auto dir = opendir(path.c_str())) {
         while (auto f = readdir(dir)){
@@ -46,12 +54,13 @@ int main(int argc, char const *argv[]){
         int _cont = 0;
         while (auto f = readdir(dir)){
             if (!f->d_name || f->d_name[0] == '.') continue;
+            if(threads[_cont % n_threads].joinable()) threads[_cont % n_threads].join();
+            threads[_cont % n_threads] = thread(&read_dataset, path + + f->d_name);
             _cont++;
-            read_dataset(path + + f->d_name);
+            if(_cout)_temp_print("Leyendo dataset\n",_cont, num_files);
         }
         closedir(dir);
     }
-
     cout <<"Total de archivos "<< num_files << endl;
     return 0;
 }
