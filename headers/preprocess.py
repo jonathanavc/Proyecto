5f8730dd57624813ec9e678e7b872761d8b0525b
Vicente Lermanda <vlermanda@MacBook-Air.local>
2022-11-30 18:37:31 -0300

    preprocesado en python

--- headers/preprocess.py
+++ headers/preprocess.py
@@ -2,17 +2,20 @@ import pandas as pd
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-from gensim.models import Word2Vec
 import re
 import sys
 import string
 import numpy as np
+from nltk.stem import WordNetLemmatizer
 
+nltk.download('wordnet')
+nltk.download('omw-1.4')
 nltk.download('stopwords')
 nltk.download('punkt')
 stopwords = set(stopwords.words('english'))
 
-print("\nName of Python script:", sys.argv[0])
+f = open(sys.argv[1],"r+")
+texto = f.read()
 
 def clean(text):
   text = str(text).lower()
@@ -24,26 +27,26 @@ def clean(text):
       f"[{re.escape(string.punctuation)}]", "", text
   )  # Remove punctuation
 
+  lemmatizer = WordNetLemmatizer()
   tokens = nltk.word_tokenize(text)  # Get tokens from text
   tokens = [t for t in tokens if not t in stopwords]  # Remove stopwords
   tokens = ["" if t.isdigit() else t for t in tokens]  # Remove digits
   tokens = [t for t in tokens if len(t) > 1]  # Remove short tokens
+  tokens = [lemmatizer.lemmatize(t) for t in tokens] # lemmatize
 
   return tokens
 
-df = dataset.copy()
+df = pd.DataFrame({"text" : clean(texto)})
 df["text"] = df["text"].fillna("")
 df["text"] = df["text"].astype(str)
-df["t_text"] = df["text"].map(lambda x: clean(x))
 
-df = df.loc[df.t_text.map(lambda x: len(x) > 0), ["text", "t_text"]]
+# df = df.loc[df.t_text.map(lambda x: len(x) > 0), ["text"]]
 
 docs = df["text"].values
-tokenized_docs = df["t_text"].values
 
-df['new_text'] = df['t_text'].apply(lambda x: ' '.join(x))
-out_file = ' '.join(df['new_text'])
+#df['new_text'] = df['text'].apply(lambda x: ' '.join(x))
+out_file = ' '.join(docs)
 
-f = open("output.txt","w+")
-f.write(out_file)
-f.close()
+out = open(sys.argv[2],"w+")
+out.write(out_file)
+out.close()
