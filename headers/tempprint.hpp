2af50463b2065637382e4ae4daa83aa0997674b8
jonathanavc <jonvenega@gmail.com>
2022-12-02 03:57:22 -0300

    Update tempprint.hpp

--- headers/tempprint.hpp
+++ headers/tempprint.hpp
@@ -4,6 +4,7 @@
 
 void temp_print(std::string _s, int _i1 = -1, int _i2 = -1, mytime * _mytime = NULL){
     double percent;
+    std::cout << "\33[2K\r";
     if( _i1!= -1){
         percent = (double)(_i1 * 100) / _i2;
         std::cout << std::fixed << std::setprecision(2) <<"["<<percent<< "%]";
@@ -12,9 +13,12 @@ void temp_print(std::string _s, int _i1 = -1, int _i2 = -1, mytime * _mytime = N
     if(_mytime!=NULL){
         double tnecrep = 100.0 - percent;
         size_t dif = _mytime->getdif();
-        std::cout << std::fixed << std::setprecision(2) << ((tnecrep * dif)/percent)/1000.0<<"s";
+        double seconds = ((tnecrep * dif)/percent)/1000.0;
+        if(seconds > 3600) std::cout << std::fixed << std::setprecision(2) << seconds/3600 <<"m";
+        else if(seconds > 60) std::cout << std::fixed << std::setprecision(2) << seconds/60 <<"m";
+        else std::cout << std::fixed << std::setprecision(2) << seconds <<"s";
+        
     }
     std::cout << "\n";
-    std::cout << "\33[2K\r";
     std::cout << "\e[A";
 }
