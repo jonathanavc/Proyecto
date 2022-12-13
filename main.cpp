0e40520119f29ec1c6d8c9921d41f2cf596cb226
jonathanavc <jonvenega@gmail.com>
2022-12-13 20:24:48 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -26,7 +26,8 @@ void read_dataset(string dir){
     auto json_file = nlohmann::json::parse(buffer);
     for (auto text : json_file){
         int _id = atoi(((string)text["id"]).c_str());
-        string _text(text["text"].get<string>());
+        string _text(text["text"].get<std::string>());
+        cout << _text;
         istringstream  words(_text);
         string word;
         float * M;
