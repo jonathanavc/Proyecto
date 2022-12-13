040c3a210a6f16afa26e8a86a43580be0c592402
jonathanavc <jonvenega@gmail.com>
2022-12-13 19:48:37 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -27,7 +27,6 @@ void read_dataset(string dir){
     for (auto text : json_file){
         int _id = atoi(((string)text["id"]).c_str());
         string _text = text["text"];
-        _text.push_back(0);
         _text = _pp.preprocess_str(_text);
         istringstream  words(_text);
         string word;
