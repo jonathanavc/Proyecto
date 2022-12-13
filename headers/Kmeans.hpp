6a5ca89446d3e205d091870d13c6360e870c4e5c
jonathanavc <jonvenega@gmail.com>
2022-12-13 18:27:35 -0300

    Update Kmeans.hpp

--- headers/Kmeans.hpp
+++ headers/Kmeans.hpp
@@ -82,8 +82,8 @@ void Kmeans::run(vector<Point> &all_points) {
 
   setInitialPoints(all_points);
 
-  cout << "Clusters initialized = " << clusters.size() << endl << endl;
-  cout << "Points with dimension = " << dimensions << endl << endl;
+  cout << "Clusters initialized = " << clusters.size()  << endl;
+  cout << "Points with dimension = " << dimensions << endl;
   cout << "Running K-Means Clustering.." << endl;
 
   int iter = 1;
@@ -97,8 +97,8 @@ void Kmeans::run(vector<Point> &all_points) {
       if(point.clusterID == nearestClusterID) continue;
 
       // Se elimina el punto actual de su cluster antiguo
-      clusters[point.clusterID].points.erase(find_if(clusters[point.clusterID].points.begin(), clusters[point.clusterID].points.end(), 
-            [point](Point p){return p.pointID == point.pointID;}));
+      if(point.clusterID != -1) clusters[point.clusterID].points.erase(find_if(clusters[point.clusterID].points.begin(), 
+      clusters[point.clusterID].points.end(), [point](Point p){return p.pointID == point.pointID;}));
 
       // Se agrega el punto a su cluster mas cercano. Su clusterID se actualiza
       clusters[nearestClusterID].addPoint(point);
