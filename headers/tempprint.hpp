83719ae63311edd9d8dbb9f7bb8c57e92a00df25
jonathanavc <jonvenega@gmail.com>
2022-12-15 04:07:00 -0300

    Update tempprint.hpp

--- headers/tempprint.hpp
+++ headers/tempprint.hpp
@@ -14,10 +14,9 @@ void temp_print(std::string _s, int _i1 = -1, int _i2 = -1, mytime * _mytime = N
     }
     std::cout << _s;
     if(_mytime!=NULL){
-        double tnecrep = 100.0 - percent;
         size_t dif = _mytime->getdif();
         double seconds;
-        if(_i1 = -1) seconds = ((tnecrep * dif)/percent)/1000.0;
+        if(_i1 = -1) seconds = ((100.0 - percent * dif)/percent)/1000.0;
         else seconds = dif/1000.0;
         if(seconds > 3600) std::cout << std::fixed << std::setprecision(2) << seconds/3600 <<"h";
         else if(seconds > 60) std::cout << std::fixed << std::setprecision(2) << seconds/60 <<"m";
