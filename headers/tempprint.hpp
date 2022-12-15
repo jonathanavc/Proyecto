f8e47aca14aa2a5a39e2a16fac739407687d1294
jonathanavc <jonvenega@gmail.com>
2022-12-15 04:09:57 -0300

    Update tempprint.hpp

--- headers/tempprint.hpp
+++ headers/tempprint.hpp
@@ -7,7 +7,7 @@
 
 void temp_print(std::string _s, int _i1 = -1, int _i2 = -1, mytime * _mytime = NULL, bool _temp = true){
     double percent;
-    if(_temp) std::cout << "\33[2K\r";
+    std::cout << "\33[2K\r";
     if( _i1!= -1){
         percent = (double)(_i1 * 100) / _i2;
         std::cout << std::fixed << std::setprecision(2) <<"["<<percent<< "%]";
