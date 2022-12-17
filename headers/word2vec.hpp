84fddb71293e53303a16fbaa854ef146baf45d98
jonathanavc <jonvenega@gmail.com>
2022-12-17 20:22:46 -0300

    Update word2vec.hpp

--- headers/word2vec.hpp
+++ headers/word2vec.hpp
@@ -47,6 +47,7 @@ word2vec::word2vec(std::string file_name, bool __cout = 1){
             if ((a < max_w) && (c[a] != '\n')) a++;
         }
         c[a] = 0;
+        sword.push_back(c[a]);
         for (a = 0; a < size; a++) fread(&M[size * cont + a], sizeof(float), 1, f);
         std::transform(sword.begin(), sword.end(), sword.begin(), [](unsigned char c){ return std::tolower(c); });
         auto it = w2v.insert({sword, cont});
