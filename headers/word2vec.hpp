a0856c0abfb43232669cf6a74cfaf5554e4aa8c0
jonathanavc <jonvenega@gmail.com>
2022-12-17 16:58:23 -0300

    Update word2vec.hpp

--- headers/word2vec.hpp
+++ headers/word2vec.hpp
@@ -89,16 +89,17 @@ std::string word2vec::getnearestword(std::vector<float> _f, int n_threads = 1){
     }*/
     std::cout << "-------------------"<< std::endl;
     //funcionará mejor?
-    //#pragma omp parallel for num_threads(n_threads)
+    #pragma omp parallel for num_threads(n_threads)
     for (size_t i = 0; i < words; i++){
-        if(_cout && i%10000 == 0)temp_print("", i, words);
         float dist;
-        #pragma omp parallel for reduction(+: dist) num_threads(n_threads)
         for (size_t j = 0; j < size; i++){
             dist += (M[i * size + j] - _f[i]) * (M[i * size + j] - _f[i]);
         }
         dist = sqrt(dist);
+        #pragma omp critical
+        {
         if(dist < min) {min = dist;pos_min = i;}
+        }
     }
     return v2w[pos_min]->first;
 }
