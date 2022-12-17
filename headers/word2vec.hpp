abf6bda1df771ce8e41b0248b8b6f1cc0b833d94
jonathanavc <jonvenega@gmail.com>
2022-12-17 16:23:41 -0300

    Update word2vec.hpp

--- headers/word2vec.hpp
+++ headers/word2vec.hpp
@@ -72,17 +72,38 @@ long long word2vec::getdim(){
 std::string word2vec::getnearestword(std::vector<float> _f, int n_threads = 1){
     if(_f.size()!= size) return "######";
     float min = FLT_MAX;
+    int pos_min = -1;
     std::string s = "";
+    /* mal rendimiento
     //#pragma omp parallel for num_threads(n_threads) //no funcionaaaaaaaaaaaaaaaaaaaaaa
-    for (std::map<std::string, int>::iterator it = std::begin(w2v); it != std::end(w2v); it++){
+    for (std::map<std::string, int>::iterator it = w2v.begin(); it != w2v.end(); it++){
         float dist = 0.0;
         #pragma omp parallel for reduction(+: dist) num_threads(n_threads)
         for (size_t i = 0; i < size; i++){
-            float res = (M[it->second +i] - _f[i]) * (M[it->second +i] - _f[i]);
+            float res = (M[it->second * size +i] - _f[i]) * (M[it->second * size +i] - _f[i]);
             dist += res;
         }
         dist = sqrt(dist);
         if(dist < min) {min = dist;s = it->first;}
+    }*/
+
+    //funcionará mejor?
+    #pragma omp parallel for num_threads(n_threads)
+    for (size_t i = 0; i < words; i++){
+        float dist;
+        for (size_t j = 0; j < size; i++){
+            dist = (M[i * size + j] - _f[i]) * (M[i * size + j] - _f[i]);
+        }
+        #pragma omp critical
+        {
+        if(dist < min) {min = dist;pos_min = i;}
+        }
+    }
+    for (std::map<std::string, int>::iterator it = w2v.begin(); it != w2v.end(); it++){
+        if(it->second == pos_min){
+            s = it->first;
+            break;
+        }
     }
     return s;
 }
